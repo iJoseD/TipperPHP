e28df2e8a4bd097de541ca6e9735d5db7593cb17
iJoseD <jcorrea@maando.com>
2021-10-19 13:36:54 -0500

    Login

--- layouts/login.php
+++ layouts/login.php
@@ -2,18 +2,18 @@
     <div class="row header">
         <div class="col-12">
             <div class="row">
-                <div class="col-12 text-center">
-                    <figure>
+                <div class="col-12">
+                    <figure class="iconotipo text-center">
                         <img src="/wp-content/uploads/2021/08/newIsotipo.svg" alt="Logo ThankYou Tipper">
                     </figure>
                 </div>
             </div>
             <div class="row welcome">
                 <div class="col-6">
-                    <p class="text-white">Welcome to</p>
+                    <p class="text-end text-white">Welcome to</p>
                 </div>
                 <div class="col-6">
-                    <figure>
+                    <figure class="logo text-start">
                         <img src="/wp-content/uploads/2021/08/newLogo-W.svg" alt="Logo ThankYou Tipper">
                     </figure>
                 </div>
