7c4285b4a2b6d1a2d7e745698525c721c0c6987c
iJoseD <jcorrea@maando.com>
2021-10-19 13:15:10 -0500

    Login

--- index.php
+++ index.php
@@ -9,17 +9,13 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="dist/style.css">
         
-        <title>Backoffice</title>
+        <title>ThankYou Tipper</title>
     </head>
     <body>
-        <!-- Navbar -->
-        <?php require_once('layouts/navbar.php'); ?>
-        
         <!-- Dashboard -->
-        <?php require_once('layouts/dashboard.php'); ?>
+        <?php require_once('layouts/login.php'); ?>
         
         <!-- Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <?php require_once('dist/requireFooter.php'); ?>
     </body>
 </html>
---
+++ layouts/login.php
@@ -0,0 +1,23 @@
+<section class="container">
+    <div class="row bgAzul">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <figure>
+                        <img src="" alt="">
+                    </figure>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <p>Welcome to</p>
+                </div>
+                <div class="col-6">
+                    <figure>
+                        <img src="" alt="">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
