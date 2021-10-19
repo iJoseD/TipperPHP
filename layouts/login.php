840b89d331aff483076377fb99d6adf53dcf28cb
iJoseD <jcorrea@maando.com>
2021-10-19 13:21:38 -0500

    Login

--- layouts/login.php
+++ layouts/login.php
@@ -4,17 +4,17 @@
             <div class="row">
                 <div class="col-12">
                     <figure>
-                        <img src="" alt="">
+                        <img src="/wp-content/uploads/2021/08/newIsotipo.svg" alt="Logo ThankYou Tipper">
                     </figure>
                 </div>
             </div>
             <div class="row">
                 <div class="col-6">
-                    <p>Welcome to</p>
+                    <p class="text-withe">Welcome to</p>
                 </div>
                 <div class="col-6">
                     <figure>
-                        <img src="" alt="">
+                        <img src="/wp-content/uploads/2021/08/newLogo-W.svg" alt="Logo ThankYou Tipper">
                     </figure>
                 </div>
             </div>
