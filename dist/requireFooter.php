21a5184154224fc08e7e67065ab187e8f0ccf9d7
iJoseD <jcorrea@maando.com>
2021-10-21 14:22:46 -0500

    Phone

--- dist/requireFooter.php
+++ dist/requireFooter.php
@@ -2,3 +2,4 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="/dist/script.js"></script>
 <script type="text/javascript" src="/dist/jquery.cookie.js"></script>
+<script type="text/javascript" src="/dist/js/intlTelInput.js"></script>
--- login/index.php
+++ login/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
     <!-- Head -->
     <?php require_once('../dist/requireHead.php'); ?>
-    <link rel='stylesheet' href='../dist/intlTelInput.min.css'>
     
     <body>
         <!-- Login -->
@@ -10,8 +9,5 @@
         
         <!-- Bootstrap Bundle with Popper -->
         <?php require_once('../dist/requireFooter.php'); ?>
-
-        <script src='../dist/intlTelInput.min.js'></script>
-        <script src='../dist/intlTelInput-jquery.min.js'></script>
     </body>
 </html>
