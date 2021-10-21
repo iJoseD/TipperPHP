b9cf1578c15deea67893d70b0890f17380e38091
iJoseD <jcorrea@maando.com>
2021-10-21 05:03:47 -0500

    Test SMS

--- controller/testSendSMS.php
+++ controller/testSendSMS.php
@@ -7,13 +7,13 @@ require_once('../aws/aws-autoloader.php');
 use Aws\Sns\SnsClient;
 use Aws\Exception\AwsException;
 
-echo 'Hola x2';
+$SnSclient = new SnsClient([
+    'profile' => 'default',
+    'region' => 'us-east-1',
+    'version' => '2010-03-31'
+]);
 
-// $SnSclient = new SnsClient([
-//     'profile' => 'default',
-//     'region' => 'us-east-1',
-//     'version' => '2010-03-31'
-// ]);
+echo 'Hola x3';
 
 // $message = 'This message is sent from a Amazon SNS code sample.';
 // $phone = '+573013808512';
