4d8d42b5a6102a60097ff82de388c4fa342f734f
iJoseD <jcorrea@maando.com>
2021-10-23 13:38:40 -0500

    Update uploadImgProfile.php

--- controller/uploadImgProfile.php
+++ controller/uploadImgProfile.php
@@ -9,23 +9,22 @@ $dbname     = "app_tipper";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 
-// Session
 $phone = $_SESSION['phone'];
-
-// Var
-$directorio = $_SERVER['DOCUMENT_ROOT'].'/dist/img-profile/';
-$nombre_img = $_FILES['imagen']['name'];
-$tipo       = $_FILES['imagen']['type'];
 
 if (is_array($_FILES) && count($_FILES) > 0) {
     if (($_FILES["file"]["type"] == "image/pjpeg")
         || ($_FILES["file"]["type"] == "image/jpeg")
         || ($_FILES["file"]["type"] == "image/png")
         || ($_FILES["file"]["type"] == "image/gif")) {
-            move_uploaded_file($_FILES['imagen']['tmp_name'], $directorio.$nombre_img);
+        if ( move_uploaded_file( $_FILES["file"]["tmp_name"], "../dist/img-profile/".$_FILES['file']['name'] ) ) {
+            $avatar = "../dist/img-profile/".$_FILES['file']['name'];
             $sql = "UPDATE userProfile SET avatar = '$avatar' WHERE phone = '$phone'";
-            $_SESSION['avatar'] = $directorio.$nombre_img;
-            echo $directorio.$nombre_img;
+            $_SESSION['avatar'] = $avatar;
+            
+            echo $avatar;
+        } else {
+            echo "error_al_mover_archivo";
+        }
     } else {
         echo "error_formato_imagen";
     }
