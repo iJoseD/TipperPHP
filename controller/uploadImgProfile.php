d5a24fddd413d6e2ec23b22268e92f210ca7ed87
iJoseD <jcorrea@maando.com>
2021-10-23 13:36:26 -0500

    Update uploadImgProfile.php

--- controller/uploadImgProfile.php
+++ controller/uploadImgProfile.php
@@ -9,22 +9,23 @@ $dbname     = "app_tipper";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 
+// Session
 $phone = $_SESSION['phone'];
+
+// Var
+$directorio = $_SERVER['DOCUMENT_ROOT'].'/dist/img-profile/';
+$nombre_img = $_FILES['imagen']['name'];
+$tipo       = $_FILES['imagen']['type'];
 
 if (is_array($_FILES) && count($_FILES) > 0) {
     if (($_FILES["file"]["type"] == "image/pjpeg")
         || ($_FILES["file"]["type"] == "image/jpeg")
         || ($_FILES["file"]["type"] == "image/png")
         || ($_FILES["file"]["type"] == "image/gif")) {
-        if ( move_uploaded_file( $_FILES["imagen"]["tmp_name"], 'https://myapp.thankyoutipper.com/dist/img-profile/'.$_FILES['imagen']['name'] ) ) {
-            $avatar = '/dist/img-profile/'.$_FILES['file']['name'];
+            move_uploaded_file($_FILES['imagen']['tmp_name'], $directorio.$nombre_img);
             $sql = "UPDATE userProfile SET avatar = '$avatar' WHERE phone = '$phone'";
-            $_SESSION['avatar'] = $avatar;
-            
-            echo $avatar;
-        } else {
-            echo "error_al_mover_archivo";
-        }
+            $_SESSION['avatar'] = $directorio.$nombre_img;
+            echo $directorio.$nombre_img;
     } else {
         echo "error_formato_imagen";
     }
