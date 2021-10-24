c753a05aa346e34e71ba963e25928b59b7f8baa2
iJoseD <jcorrea@maando.com>
2021-10-24 13:32:53 -0500

    Update my-qr.php

--- layouts/my-qr.php
+++ layouts/my-qr.php
@@ -2,14 +2,14 @@
 
 <section class="container">
     <div class="row mt-5">
-        <div class="col-12">
-            <img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper">
+        <div class="col-12 text-center">
+            <img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo-W.svg" width="60%" alt="Logo ThankYou Tipper">
         </div>
     </div>
 
-    <div class="row mt-3">
+    <div class="row mt-4">
         <div class="col-12">
-            <p class="text-white">I'm <?php echo $_SESSION['firstName']; ?> and my super power is:</p>
+            <p class="text-white" style="font-weight: 800;">I'm <?php echo $_SESSION['firstName']; ?> and my super power is:</p>
         </div>
     </div>
 
@@ -21,9 +21,9 @@
     <div class="row">
         <div class="col-1"></div>
         <div class="card col-10">
-            <div class="card-body">
+            <div class="card-body text-center">
                 <img src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=https://myapp.thankyoutipper.com/payments/?id=<?php echo $_SESSION['phone']; ?>&choe=UTF-8" alt="Generate QR Code">
-                <h5 class="card-title text--orange"><?php echo $_SESSION['firstName']; ?></h5>
+                <h5 class="card-title text--orange" style="font-weight: 800;"><?php echo $_SESSION['firstName']; ?></h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
