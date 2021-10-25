974debe413916e734f6f8a1b2ed61e1b07477b19
iJoseD <jcorrea@maando.com>
2021-10-24 19:56:38 -0500

    Navbar

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,22 +1,3 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a href="/"><img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper" width="30%;"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile/">Profile</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
 <nav>
     <div class="container">
         <div class="row">
