12393b4c8ad707f423273e35527330bb49259b0c
iJoseD <jcorrea@maando.com>
2021-10-24 19:53:50 -0500

    Navbar

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -21,11 +21,14 @@
     <div class="container">
         <div class="row">
             <div class="col-2">
-                <img src="/dist/img/menu.png" alt="icoMenu">
+                <img src="/dist/img/menu.png" width="70%" alt="icoMenu" id="icoMenu">
             </div>
-            <div class="col-10 text-center">
+            <div class="col-8 text-center">
                 <a href="/"><img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper" width="30%;"></a>
             </div>
+            <div class="col-2"></div>
         </div>
     </div>
 </nav>
+
+<aside id="menu"></aside>
