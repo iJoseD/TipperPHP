f3ddc31da6c2d4565a9dfe1d34cf6f4e72b2df02
iJoseD <jcorrea@maando.com>
2021-10-24 21:01:29 -0500

    Navbar

--- my-qr/index.php
+++ my-qr/index.php
@@ -4,6 +4,19 @@
     <?php require_once('../dist/requireHead.php'); ?>
 
     <body class="bgAzul">
+        <!-- Navbar -->
+        <nav class="mt-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="/dist/img/menuWhite.png" width="70%" alt="icoMenu" id="icoMenu">
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <aside id="menu"></aside>
+        
         <!-- My QR -->
         <?php require_once('../layouts/my-qr.php'); ?>
         
