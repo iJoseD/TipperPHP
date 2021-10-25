1bc46bc59d305decf67a90c08da21970f88d79a3
iJoseD <jcorrea@maando.com>
2021-10-24 20:57:12 -0500

    Update navbar.php

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,11 +1,11 @@
-<nav>
+<nav class="mt-3">
     <div class="container">
         <div class="row">
             <div class="col-2">
                 <img src="/dist/img/menu.png" width="70%" alt="icoMenu" id="icoMenu">
             </div>
             <div class="col-8 text-center">
-                <a href="/"><img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper" width="30%;"></a>
+                <a href="/"><img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper" width="50%;"></a>
             </div>
             <div class="col-2"></div>
         </div>
